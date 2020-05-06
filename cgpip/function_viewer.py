f352755cbc20f4bfd8dd8962e6b51d7765796ab3
Kévin <cortacero.k31130@gmail.com>
2020-05-06 15:30:35 +0200

    first step to new functions + first try to optimize existing functions

--- cgpip/function_viewer.py
+++ cgpip/function_viewer.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 
-from functions import Functions
+#from functions import Functions
+from supp_functions import SuppFunctions
 
 from skimage import data
 
 class FunctionViewer(object):
-    def __init__(self, function_list):
-        self.function_list = function_list
+    def __init__(self):
+        self.function_bundle = SuppFunctions()
+        self.function_list = range(self.function_bundle.min_function_index, self.function_bundle.max_function_index+1)
+        
 
     def run(self, img0, img1, p0, p1, p2, p3, p4):
         fig, axs = plt.subplots(len(self.function_list), 3, figsize=(15, len(self.function_list)*3))
         for i, function in enumerate(self.function_list):
-            img_out = Functions.execute(function, img0, img1, p0, p1, p2, p3, p4)
+            img_out = self.function_bundle.execute(function, img0, img1, p0, p1, p2, p3, p4)
+            print(img_out.shape)
             axs[i, 0].imshow(img0, cmap=plt.get_cmap('gray'))
             axs[i, 1].imshow(img1, cmap=plt.get_cmap('gray'))
             axs[i, 2].imshow(img_out, cmap=plt.get_cmap('gray'))
@@ -23,4 +27,4 @@ if __name__ == "__main__":
     src_img = data.astronaut()
     img0 = src_img[:, :, 0]
     img1 = src_img[:, :, 1]
-    FunctionViewer(range(4, 51)).run(img0, img1, 64, 0, 0, 0, 0)
+    FunctionViewer().run(img0, img1, 8, 8, 8, 0, 0)
